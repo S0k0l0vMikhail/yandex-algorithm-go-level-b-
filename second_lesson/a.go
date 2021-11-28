c71bf8e81b19610d4c8befc4ef0ab10f5077f283
Mikhail <dolgopeh@gmail.com>
2021-11-28 19:49:43 +0300

    second lesson A

--- second_lesson/a.go
+++ second_lesson/a.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	var a int
-	a = 1
-	fmt.Println(a)
-	fmt.Println(a)
+	var max, maxCount int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		if num == 0 {
+			break
+		}
+		if num > max {
+			max = num
+			maxCount = 1
+		} else if num == max {
+			maxCount++
+		}
+	}
+	fmt.Println(maxCount)
 }
